37982f42b4009f06e3f404d4d9e24ed59fa98709
augustosilveriosoares <falecomsilverio@gmail.com>
2024-09-05 22:40:28 -0300

    feat(openapi swagger) adicionado openapi swagger para iniciar documentacao do microservico

--- src/main/java/com/augusto/controller/CambioController.java
+++ src/main/java/com/augusto/controller/CambioController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.augusto.model.Cambio;
 import com.augusto.service.CambioService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
@@ -21,6 +23,7 @@ public class CambioController {
 	@Autowired
 	private CambioService cambioService;
 	
+	@Operation(summary = "Retorna o valor convertido com base no cambio", method = "GET")	
 	@GetMapping(value="/{amount}/{from}/{to}")
 	public Cambio getCambio( @PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to ) {
 		var cambio = cambioService.findByFromAndTo(from, to);
